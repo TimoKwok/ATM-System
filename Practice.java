2c2294f1214d0089565398d6df14c681284ffab7
TimoKwok <timothy.kwok@torontomu.ca>
2024-02-01 14:32:51 -0500

    ATM System java files for SDLC project for CPS406

---
+++ Practice.java
@@ -0,0 +1,17 @@
+
+import java.text.DecimalFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Practice{
+    static DecimalFormat f = new DecimalFormat("##.00");
+    static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("'DATE: 'yyyy/MM/dd ' TIME:' HH:mm:ss");
+
+    private static int[] allBills = {200, 200, 200};
+
+
+    public static void main(String[] args){
+        
+    
+}
